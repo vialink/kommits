633e388d6a3571dd92f06d56556827bb97661e3f
Jan Segre <jan@vialink.com.br>
2012-09-08 14:40:46 -0300

    Some small refactoring.

--- kommits/hgrepo.py
+++ kommits/hgrepo.py
@@ -6,30 +6,30 @@ from mercurial.node import short
 from models import Repo, Commit
 
 
-def find_hg_repos(repopath):
-    """Returns a list of hg Repos in repopath"""
-    def _find_repos(repopath, name):
-        if not path.isdir(repopath):
+def find_hg_repos(basepath):
+    """Returns a list of hg Repos in basepath"""
+    def _find_repos(basepath, name):
+        if not path.isdir(basepath):
             return []
         ret = []
-        subdir = listdir(repopath)
+        subdir = listdir(basepath)
         if '.hg' not in subdir:
             for r in subdir:
                 reponame =  '{0}/{1}'.format(name, r) if name else r
-                ret.extend(_find_repos( path.join(repopath, r), reponame))
+                ret.extend(_find_repos( path.join(basepath, r), reponame))
         else:
             ret.append(Repo(
                 name=name,
-                path=repopath,
+                basepath=basepath,
                 type='hg',
                 baseurl='',
                 commits=[],
             ))
         return ret
-    return _find_repos(repopath, '')
+    return _find_repos(basepath, '')
 
 
-def find_commits(repo, from_date, until_date):
+def find_hg_commits(repo, from_date, until_date):
     """Initializes repo.commits with all commits between the given dates"""
     pass
 
--- kommits/models.py
+++ kommits/models.py
@@ -1,11 +1,15 @@
 from collections import namedtuple
+from os.path import join
 
-_Repo = namedtuple('Repo', 'name path type baseurl commits')
+_Repo = namedtuple('Repo', 'name basepath type baseurl commits')
 _Commit = namedtuple('Commit', 'id url user date branch comment')
 
 
 class Repo(_Repo):
-    pass
+
+    @property
+    def path(self):
+        return join(self.basepath, self.name)
 
 
 class Commit(_Commit):
