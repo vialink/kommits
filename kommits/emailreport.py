6935baa9d2c7d75d7500eeda05d5e79605e282ff
Jan Segre <jan@vialink.com.br>
2012-09-08 20:32:31 -0300

    Implementing report utilities.

--- kommits/emailreport.py
+++ kommits/emailreport.py
@@ -1,5 +1,6 @@
 from sys import stderr
 from os import path
+from datetime import datetime, timedelta
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -32,4 +33,25 @@ def render_report(from_date, until_date):
     l = FileSystemLoader(BASEDIR)
     t = l.load(J2ENV, 'report.html')
     return t.render(withcommits=withcommits, nocommits=nocommits)
+
+
+def render_email(from_date, until_date):
+    """Same as render_report but with email headers."""
+    dateformat = '%Y-%m-%d %H:%M:%S'
+    header = '\n'.join((
+        #'From: noreply@vialink.com.br',
+        #'Reply-to: noreply@vialink.com.br',
+        #'Return-path: noreply@vialink.com.br',
+        'Subject: Commits ({0} to {1})'.format(from_date.strftime(dateformat), until_date.strftime(dateformat)),
+        'Content-Type: text/html; charset="utf-8"',
+    ))
+    return '{0}\n\n{1}'.format(header, render_report(from_date, until_date))
+
+
+def render_daily_email():
+    """Same as render_email but using the last 24h timespan."""
+    until_date = datetime.now()
+    from_date = until_date - timedelta(days=1)
+    return render_email(from_date, until_date)
+
 
